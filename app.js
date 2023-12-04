ff2812378d27fafaad7db697fb5964641cd60193
Kourtney Smith <klsmith5370@gmail.com>
2023-12-04 12:10:06 -0500

    Created route for updating a book

--- app.js
+++ app.js
@@ -77,6 +77,34 @@ app.get("/books/:id", async(req, res) => {
     }
 });
 
+// route for updating a book 
+app.put("/books/:id", async(req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+
+        const { id } = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).send({ message: "Book not found" });
+        }
+
+        return res.status(200).send({ message: "Book updated successfully" });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: error.message });
+    }
+})
+
 
 mongoose
 .connect(mongoDBURL)
