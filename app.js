8f315872ca3f29d1dd8f1f207507c546f9f862f5
Kourtney Smith <klsmith5370@gmail.com>
2023-12-04 16:54:45 -0500

    Set up CORS Policy

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ import express from "express";
 import { PORT, mongoDBURL} from "./server/config.js";
 import mongoose from "mongoose";
 import books from "./server/routes/books.js";
+import cors from "cors"
 
 const app = express();
 
 // Middleware for parsing JSON data
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: allow all origins with default of cors(*)
+// app.use(cors());
+
+// **Option 2: allow custom origins**
+app.use(
+    cors({
+        origin: "http://localhost:5555",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
 
 app.get("/", (req, res) => {
     console.log(req);
