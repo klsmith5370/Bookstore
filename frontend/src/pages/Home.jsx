daaaa3a6211ebb7e52a1cb432d62dd6586ee6afc
Kourtney Smith <klsmith5370@gmail.com>
2023-12-18 12:23:37 -0500

    Figured out bug where data couldn't be read when mapping

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,30 +4,30 @@ import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete, MdPhoneBluetoothSpeaker } from "react-icons/md";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 const Home = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
 
-       useEffect(() => {
-        setLoading(true);
-        axios
-            .get("http://localhost:5555/books")
-            .then((res) => {
-                setBooks(res.data.data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.log(error);
-                setLoading(false);
-            })
-       }, []); 
+    useEffect(() => {
+    setLoading(true);
+    axios
+        .get("http://localhost:5555/books")
+        .then((res) => {
+            setBooks(res.data);
+            setLoading(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+        })
+    }, []); 
 
     return (
         <div className="p-4">
             <div className="flex justify-between items-center">
-                <h1 className="text-3xl my-8">My Books List</h1>'
+                <h1 className="text-3xl my-8">My Books List</h1>
                 <Link to="/books/create">
                     <MdOutlineAddBox className="text-sky-800 text-4xl" />
                 </Link>
@@ -47,7 +47,7 @@ const Home = () => {
                     </thead>
 
                     <tbody>
-                        {books.map((book, index) => {
+                        {books.map((book, index) => (
                             <tr key={book._id} className="h-8">
                                 <td className="border border-slate-700 rounded-md text-center">
                                     {index + 1}
@@ -77,7 +77,7 @@ const Home = () => {
                                     </div>
                                 </td>
                             </tr>
-                        })}
+                        ))}
                     </tbody>
                 </table>
             )}
